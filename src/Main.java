347fef3e4259211ca91dfefb0ccbc9819a7ffcf8
SaiSuneetha <32009646+SaiSuneetha@users.noreply.github.com>
2017-09-16 14:33:49 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+Student[1]=new Student("vineetha","1997-02-09",95.32);
+Student[2]=new Student("pradeep","1988-10-26",80.265);
+	}
+
+}
